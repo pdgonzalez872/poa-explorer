eec1a969ab928f5f57b4a37b38aa79a2618c80c5
Amanda Sposito <amanda.sposito@plataformatec.com.br>
2018-05-15 13:58:10 -0300

    Add page to verify contract

--- apps/explorer_web/lib/explorer_web/router.ex
+++ apps/explorer_web/lib/explorer_web/router.ex
@@ -59,6 +59,13 @@ defmodule ExplorerWeb.Router do
         only: [:index],
         as: :internal_transaction
       )
+
+      resources(
+        "/contract",
+        AddressVerifyContractController,
+        only: [:new, :create],
+        as: :verify_contract
+      )
     end
 
     get("/search", ChainController, :search)
