1beb4d6609bbb61569cd35c61682d24a4e5e8044
Luke Imhoff <luke.imhoff@dockyard.com>
2018-05-17 15:37:37 -0500

    fixup! Convert blocks nonce to bytea

--- apps/explorer/priv/repo/migrations/20180517194010_alter_blocks_nonce_from_integer_to_bytea.exs
+++ apps/explorer/priv/repo/migrations/20180517194010_alter_blocks_nonce_from_integer_to_bytea.exs
@@ -2,6 +2,6 @@ defmodule Explorer.Repo.Migrations.AlterBlocksNonceFromIntegerToBytea do
   use Ecto.Migration
 
   def change do
-    execute "ALTER TABLE blocks ALTER COLUMN nonce TYPE bytea USING decode(lpad(to_hex(nonce), 16, '0'), 'hex')"
+    execute("ALTER TABLE blocks ALTER COLUMN nonce TYPE bytea USING decode(lpad(to_hex(nonce), 16, '0'), 'hex')")
   end
 end
